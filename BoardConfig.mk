641c4c9ac9734aab7431e8019c434a94713e3544
Artem Borisov <dedsa2002@gmail.com>
2017-12-30 16:03:20 +0300

    lavender: Enable boot and system server dex-preopt
    As stated in https://android.googlesource.com/platform/build/+/418258c
    this is required in O-MR1 since sepolicy now prevents system server from
    loading anything from /data making our system extremely slow and janky.
    
    Change-Id: I215ec7ebe95981ef0b953fe938e12b83675795ab

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -268,10 +268,12 @@ TARGET_USE_SDCLANG := true
 # Dex
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
-    WITH_DEXPREOPT ?= true
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+    endif
   endif
 endif
-WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY ?= true
 
 # Inherit from the proprietary version
 -include vendor/xiaomi/lavender/BoardConfigVendor.mk
