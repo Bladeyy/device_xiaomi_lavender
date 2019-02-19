5f224555d21f39ba9fd901a6d4cf2012f38a3da9
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-02-19 10:03:20 -0300

    lavender: Allow active package broadcast

--- device.mk
+++ device.mk
@@ -34,6 +34,7 @@ TARGET_SCREEN_WIDTH := 1080
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-pe
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
