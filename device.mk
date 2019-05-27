988265ab2edc92622478f539c18ce8f98059f2d5
Bladeyy <getsuga369@gmail.com>
2019-05-27 22:30:16 +0530

    lavender: Build separate system and vendor HIDL libs

--- device.mk
+++ device.mk
@@ -229,7 +229,10 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0
+    android.hidl.base@1.0 \
+    android.hidl.base@1.0_system \
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0_system
 
 # IFAA manager
 PRODUCT_PACKAGES += \
