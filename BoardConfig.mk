ccb755edca993c0fd2c176e8a7d82c4a6618d620
Bladeyy <getsuga369@gmail.com>
2019-05-27 15:59:03 +0530

    lavender: Import stock manifest and vendor_framework_compatibility_matrix file

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -105,6 +105,12 @@ BOARD_USE_LEGACY_UI := true
 TARGET_NO_RPC := true
 USE_DEVICE_SPECIFIC_GPS := true
 
+# HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/vendor_framework_compatibility_matrix.xml
+DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+
 # Init
 TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
 
