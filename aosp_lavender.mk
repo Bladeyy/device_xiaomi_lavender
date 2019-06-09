2b7acca5980bbf80a72688e33e19274361d12fdc
Bladeyy <getsuga369@gmail.com>
2019-06-09 11:48:12 +0530

    lavender: Initial changes for caf

--- aosp_lavender.mk
+++ aosp_lavender.mk
@@ -15,7 +15,7 @@ $(call inherit-product, device/xiaomi/lavender/device.mk)
 # Inherit some common PixelExperience stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_lavender
