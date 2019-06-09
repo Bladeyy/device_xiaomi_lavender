0d40dfa3470effa4a1d9f8fe16e9aa5216c34895
Bladeyy <getsuga369@gmail.com>
2019-06-09 11:34:50 +0530

    Initial commit for PixelExperience

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
 #
-# Copyright (C) 2019 The LineageOS Project
+# Copyright (C) 2019 The PixelExperience Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lavender.mk
+    $(LOCAL_DIR)/aosp_lavender.mk
